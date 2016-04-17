f83720fa632ba4645537893aed43fa428a46ad1a
d54580066305 <zousulong@gamil.com>
2016-04-17 15:36:19 +0800

    by zsl

--- app/src/main/java/com/zou/dragony/MainActivity.java
+++ app/src/main/java/com/zou/dragony/MainActivity.java
@@ -1,7 +1,7 @@
 package com.zou.dragony;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //这一行 邹苏启加的  邹苏隆 在下面家一行随便的注释
+        //可以睡觉了
     }
 }
