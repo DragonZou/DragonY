b05a5903542f0d1e14214b716dbdeb48fa84da31
zousuqi <lszousuqi@163.com>
2016-04-19 22:47:04 +0800

    by zsq

--- app/src/main/java/com/zou/dragony/MainActivity.java
+++ app/src/main/java/com/zou/dragony/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //4月19号新加一行
         //这一行 邹苏启加的  邹苏隆 在下面家一行随便的注释
     }
 }
