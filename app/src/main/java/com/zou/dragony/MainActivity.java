39cfe49a86960940f64f8f3cf077a233156ebc2c
d54580066305 <zousulong@gamil.com>
2016-04-20 19:05:14 +0800

    7点

--- app/src/main/java/com/zou/dragony/MainActivity.java
+++ app/src/main/java/com/zou/dragony/MainActivity.java
@@ -10,15 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //4月19号新加一行 的
-        //这一行 邹苏启加的  邹苏隆 在下面家一行随便的注释
-        //可以睡觉了
-        //4月19日传的
-        //新加的
-        //11点加的
-        //aaa
-        //new brach zsl a
-        //4月20日
-        //7点
+        
         
     }
 }
