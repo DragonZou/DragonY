41615365fd0f0b689da98ae6f94245e2fadfe202
zousuqi <lszousuqi@163.com>
2016-04-20 23:23:42 +0800

    by zsq

--- app/src/main/java/com/zou/app/utils/ToastUtil.java
+++ app/src/main/java/com/zou/app/utils/ToastUtil.java
@@ -1,7 +1,34 @@
 package com.zou.app.utils;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
+ * Toast工具类
  * Created by zousuqi on 16/4/16.
  */
 public class ToastUtil {
+
+    private static Toast toast;
+
+    public static void show(Context c,int msg){
+        if(toast == null){
+            toast = Toast.makeText(c,msg,Toast.LENGTH_LONG);
+        }else {
+            toast.setText(msg);
+        }
+
+        toast.show();
+    }
+
+    public static void show(Context c,CharSequence msg){
+        if(toast == null){
+            toast = Toast.makeText(c,msg,Toast.LENGTH_LONG);
+        }else {
+            toast.setText(msg);
+        }
+
+        toast.show();
+    }
+
 }
